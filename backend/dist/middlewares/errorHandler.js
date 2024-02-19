a185260caff719b915ccc7c9c7aefdf7e5475e05
jamaloflagos <toyinjamal@gmail.com>
2024-02-19 12:11:03 +0100

    Fixing bugs - Ensure all questions are answered and throw an error if there is an unaswered question

--- backend/dist/middlewares/errorHandler.js
+++ backend/dist/middlewares/errorHandler.js
@@ -2,10 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const { logEvents } = require('../middlewares/logger');
 const errorHandler = (err, req, res, next) => {
-    logEvents(`${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers}`, 'errLog.txt');
-    const status = res.statusCode ? res.statuscode : 500;
+    logEvents(`${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers}`);
+    const status = res.statusCode !== undefined ? res.statusode : 500;
     res.status(status);
     res.json({ message: err.message, isError: true });
 };
 module.exports = errorHandler;
- 
--- backend/dist/public/js/entranceExam.js
+++ backend/dist/public/js/entranceExam.js
@@ -1,46 +1,43 @@
 function submitAnswers(email) {
     // Collect answers for each question
     // const answers = {};
+    const errParagraph = document.querySelector('#err');
     const data = {
         email: email,
         answers: []
     };
-    let allQuestionsAnswered = true;
     // Collect answers for each question
+  try {
     for (let i = 1; i <= 20; i++) {
         const radioButtons = document.getElementsByName(i);
+        // console.log(radioButtons);
         let selectedAnswer = null;
-        let buttonChecked = false;
         radioButtons.forEach(button => {
             // if (!button.checked) {
             //     break console.log('Answer all question')
             // }
+            console.log(button.checked);
             if (button.checked) {
                 console.log('question answered');
                 selectedAnswer = button.value;
-                buttonChecked = true;
-            } 
+            }  
 
-            if (!button.checked) {
-                console.log('Answer all question');
-            }
+            // if(!button.checked) throw new Error('Answer all question');
+
         });
         if (selectedAnswer !== null) {
             // answers[i] = selectedAnswer;
-            console.log('answer not null');
             const obj = {id: i, selectedAnswer: selectedAnswer};
             data.answers.push(obj);
-        } else {
-            console.log('answer is null');
-            allQuestionsAnswered = false;
-        }
+        } 
     }
-
-    if (allQuestionsAnswered) {
-        console.log(data, email);
-    } else {
-        console.log('Answer all questions');
-    }
+    console.log(data, email);
+    if (data.answers.length < 10) throw new Error('Answer all question');
+  } catch (err) {
+    console.log(err.message);
+    errParagraph.textContent = err.message;
+  }
+    
     // Send answers to the server (you would customize this part)
     // fetch('/submit-answers', {
     //     method: 'POST',

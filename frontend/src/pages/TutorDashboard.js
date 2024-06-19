28637d78cf27850b629d38da6312e89b60768b9f
jamaloflagos <toyinjamal@gmail.com>
2024-06-19 12:58:24 +0100

    setting up

--- frontend/src/pages/TutorDashboard.js
+++ frontend/src/pages/TutorDashboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import StudentProgressOverview from './StudentProgressOverview';
 import RecentSubmissionsList from './RecentSubmissionsList';
 import UpcomingDeadlines from './UpcomingDeadlines';
